7182e028c98a7deb00a49bbd88134e0322d5e4a8
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-11 22:27:13 +0100

    adding useContext hook as an alternative to <MyContext.Consumer/> Component

--- src/context/ContextCard.js
+++ src/context/ContextCard.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import MyContext from "../context/context";
 
 const ContextCard = (props) => {
+  const context = useContext(MyContext);
+
   return (
     <div>
-      <MyContext.Consumer>
+      <h3>Name: {context.profile.username}</h3>
+      <h3>LastName: {context.profile.lastname}</h3>
+      <h3>Age: {context.profile.age}</h3>
+      <button onClick={() => context.hey()}>Alert</button>
+      <button onClick={() => context.incAge()}>Add Age</button>
+
+      {/* <MyContext.Consumer>
         {(context) => (
           <>
             <h3>Name: {context.profile.username}</h3>
@@ -14,7 +22,7 @@ const ContextCard = (props) => {
             <button onClick={ ()=> context.incAge()}>Add Age</button>
           </>
         )}
-      </MyContext.Consumer>
+      </MyContext.Consumer> */}
 
       {/* <h3>Name: {props.profile.username}</h3>
             <h3>LastName: {props.profile.lastname}</h3>
