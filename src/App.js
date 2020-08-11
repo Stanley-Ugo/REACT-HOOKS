54637bcbb8aea943f87371f1c2426cd691ce2098
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-11 17:16:31 +0100

    adding a Posts & usePosts array of objects with useState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-
-const App = ({initialCount}) =>  {
-
+const App = ({ initialCount }) => {
   let [count, setCount] = useState(initialCount);
+  let [posts, setPosts] = useState([
+    {
+      name: "Super awesome hooks",
+      body: "Everything is awesome when you are part of a team",
+    },
+  ]);
 
-  const addOne = () =>{
-    setCount( prevCount => {
-      return prevCount + 1
-    })
-  }
+  const addOne = () => {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  };
 
-  const restOne = () =>{
-    setCount( prevCount => {
-      return prevCount - 1
-    })
-  }
+  const restOne = () => {
+    setCount((prevCount) => {
+      return prevCount - 1;
+    });
+  };
 
   return (
     <>
       <h3>Count: {count}</h3>
       <button onClick={addOne}>Add one +</button>
       <button onClick={restOne}>Rest one -</button>
-      <button onClick={()=> setCount(0)}>Reset</button>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <hr />
+      {posts.map((item, i) => (
+        <div key={i}>
+          <div>Name: {item.name}</div>
+          <div>Body: {item.body}</div>
+        </div>
+      ))}
     </>
   );
-}
+};
 
 export default App;
