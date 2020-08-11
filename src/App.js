f002afe67767a3a5e66a971ab7388a43b458aa10
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-11 01:48:12 +0100

    adding a click event that triggers restOne for deducting the count by one

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 
 
-const App = () =>  {
+const App = ({initialCount}) =>  {
 
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState(initialCount);
 
   const addOne = () =>{
-    setCount(count + 1)
+    setCount( prevCount => {
+      return prevCount + 1
+    })
+  }
+
+  const restOne = () =>{
+    setCount( prevCount => {
+      return prevCount - 1
+    })
   }
 
   return (
     <>
       <h3>Count: {count}</h3>
       <button onClick={addOne}>Add one +</button>
+      <button onClick={restOne}>Rest one -</button>
     </>
   );
 }
