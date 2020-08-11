0fc5adc6a0de2e718a355d9453e1ab8c85910850
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-11 19:16:48 +0100

    dispatching actions for 'decrement' & 'clear'

--- src/useReducer.js
+++ src/useReducer.js
@@ -4,7 +4,11 @@ import React,{ useReducer } from "react";
 function reducer(state,action){
     switch(action.type){
         case 'increment':
-            return { count: state.count + 1 }
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1};
+        case 'clear':
+            return { count: action.payload }
         default:
             throw new Error();
     }
@@ -12,16 +16,16 @@ function reducer(state,action){
 
 const ReducerComp = ({ initialCount }) => {
 
-    const initialState = {count: initialCount}
+    //const initialState = {count: initialCount}
   
-    const [state,dispatch] = useReducer(reducer, initialState);
+    const [state,dispatch] = useReducer(reducer, {count: initialCount});
 
   return (
     <>
       <h3>Count: {state.count}</h3>
       <button onClick={()=> dispatch({ type: 'increment'})}>Add one +</button>
-      <button >Rest one -</button>
-      <button >Reset</button>
+      <button onClick={()=> dispatch({ type: 'decrement'})}>Rest one -</button>
+      <button onClick={ ()=> dispatch({type: 'clear',payload: initialCount})}>Reset</button>
       
     </>
   );
